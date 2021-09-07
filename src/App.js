0b94bc63119a305f0054404d78af6b6bec3dfd8d
Journey Cruz <journey.cruz@gmail.com>
2021-09-07 12:27:52 -0500

    Revert "App.js html structure changes"
    This reverts commit f6ae94640ba72fe89ee110b07864c87876805d2b.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
   return (
     <main className='wrapper'>
       <section className='top-container'>
-        <header className='graph'>
-          <StatChart />
-        </header>
+      <header className='graph'>
+        <StatChart />
+      </header>
       </section>
       <section className='bottom-container'>
-        <div className='movies movie-a'>
-          <PurchaseOption title='Movie 1' />
-        </div>
-        <div className='movies movie-b'>
-          <PurchaseOption title='Movie 2' />
-        </div>
+      <div className='movies movie-a'>
+        <PurchaseOption title='Movie 1' />
+      </div>
+      <div className='movies movie-b'>
+        <PurchaseOption title='Movie 2' />
+      </div>
       </section>
     </main>
   );
