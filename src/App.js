6d745978c8ed06185ad46dfc9f81991fc606adbd
Journey Cruz <journey.cruz@gmail.com>
2021-09-07 14:46:57 -0500

    Created responsive layout for dashboard using CSS Grid, Flexbox, and size requirements from design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import StatChart from "./components/StatChart";
-import PurchaseOption from "./components/PurchaseOption";
-import "./styles/App.scss";
+import { Component, React } from 'react';
+import './styles/App.scss';
 
-function App() {
-  return (
-    <main className='wrapper'>
-      <section className='top-container'>
-      <header className='graph'>
-        <StatChart />
-      </header>
-      </section>
-      <section className='bottom-container'>
-      <div className='movies movie-a'>
-        <PurchaseOption title='Movie 1' />
-      </div>
-      <div className='movies movie-b'>
-        <PurchaseOption title='Movie 2' />
-      </div>
-      </section>
-    </main>
-  );
+// Components
+// Data
+import { dataset1 } from './data/Movies';
+
+// Extra Dataset for presentation
+// import { dataset2 } from './data/Movies';
+
+const dataset = dataset1;
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      movieOne: dataset.movie1,
+      movieTwo: dataset.movie2,
+      location: dataset.location,
+      update: dataset.lastUpdate
+    };
+  }
+
+  render () {
+    return (
+      <main className='wrapper'>
+        <section className='top-container'>
+          <header className='info-card'>
+            {/* InfoPanel */}
+          </header>
+        </section>
+        <section className='bottom-container'>
+          <div className='movies'>
+            {/* Purchase option */}
+          </div>
+          <div className='movies'>
+            {/* Purchase option */}
+            </div>
+        </section>
+      </main>
+    );
+  }
 }
 
 export default App;
